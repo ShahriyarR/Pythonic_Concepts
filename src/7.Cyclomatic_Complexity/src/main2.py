1592a71665c5cc2580b3de5cec3497cf80145df2
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2022-08-31 11:05:13 +0400

    Cyclomatic complexity code is ready to go

---
+++ src/7.Cyclomatic_Complexity/src/main2.py
@@ -0,0 +1,50 @@
+import os
+
+from typing import Union
+from PIL import Image
+
+def __open_image(src_img: str) -> Image.Image:
+    ...
+
+def _get_opened_image(src_img: str) -> Image.Image:
+    # Sanity Check if the path exists
+    if not os.path.isfile(src_img):
+        raise FileNotFoundError("Path not found")
+    try:
+        # Open the image at given path
+        img = __open_image(src_img)
+    except:
+        # File is not an Image
+        raise Exception("Wrong format")
+    else:
+        return img
+
+
+def __fetch_image(
+    self, src_img: Union[str, Image.Image], gif_allowed: bool
+) -> Image.Image:
+
+    if isinstance(src_img, str) and (gif_allowed or not src_img.endswith(".gif")):
+
+        img = _get_opened_image(src_img=src_img)
+
+    elif isinstance(src_img, Image.Image) and (
+        gif_allowed or not self.__isgif(src_img)
+    ):
+
+        if src_img.format == "GIF":
+            # Do not convert GIF file
+            return src_img
+        elif src_img.format == "PNG":
+            # Convert the Image to RGBA if it's png
+            img = src_img.convert("RGBA")
+        else:
+            # Otherwise convert it to RGB
+            img = src_img.convert("RGB")
+    else:
+        # File is not an Image
+        # OR it's a GIF but GIF is not allowed
+
+        # Raise the GENERIC exception here
+        raise Exception("Wrong format")
+    return img
---
+++ src/7.Cyclomatic_Complexity/src/main3.py
@@ -0,0 +1,59 @@
+import os
+
+from typing import Union
+from PIL import Image
+from collections import defaultdict
+
+
+def __open_image(src_img: str) -> Image.Image:
+    ...
+
+def __isgif(src_img: Image.Image) -> bool:
+    ...
+
+def _get_opened_image(src_img: str) -> Image.Image:
+    # Sanity Check if the path exists
+    if not os.path.isfile(src_img):
+        raise FileNotFoundError("Path not found")
+    try:
+        # Open the image at given path
+        img = __open_image(src_img)
+    except:
+        # File is not an Image
+        raise Exception("Wrong format")
+    else:
+        return img
+
+def _convert_to_rgb_or_rgba(src_img: Image.Image) -> Image.Image:
+
+    _map = defaultdict(lambda: lambda: src_img.convert("RGB"))
+    _map["PNG"] = lambda: src_img.convert("RGBA")
+    return _map[src_img.format]()
+
+
+
+def __fetch_image(
+    self, src_img: Union[str, Image.Image], gif_allowed: bool
+) -> Image.Image:
+
+    if isinstance(src_img, str) and (gif_allowed or not src_img.endswith(".gif")):
+
+        img = _get_opened_image(src_img=src_img)
+
+    elif isinstance(src_img, Image.Image) and (
+        gif_allowed or not __isgif(src_img)
+    ):
+
+        if src_img.format == "GIF":
+            # Do not convert GIF file
+            return src_img
+        
+        img = _convert_to_rgb_or_rgba(src_img=src_img)
+
+    else:
+        # File is not an Image
+        # OR it's a GIF but GIF is not allowed
+
+        # Raise the GENERIC exception here
+        raise Exception("Wrong format")
+    return img
